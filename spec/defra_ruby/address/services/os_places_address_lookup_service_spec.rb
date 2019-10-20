067e55e449013e8ff1db485c37b18b1220b9b64c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-10-20 21:34:10 +0100

    Replace VCR with stubs
    We do not expect the API's of the services we are calling to change. When they do we'll add a new service to this gem. So using VCR and having to re-record cassettes seems an unnecessary overhead so this change moves to stubing the request instead.

--- spec/defra_ruby/address/services/os_places_address_lookup_service_spec.rb
+++ spec/defra_ruby/address/services/os_places_address_lookup_service_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "rest-client"
 
 module DefraRuby
   module Address
@@ -11,13 +12,44 @@ module DefraRuby
         end
 
         context "when the postcode is valid" do
-          before { VCR.insert_cassette("os_places_postcode_valid") }
-          after { VCR.eject_cassette }
-
+          let(:url) { "http://localhost:8005/addresses?postcode=#{postcode}" }
           let(:postcode) { "BS1 5AH" }
+
+          before do
+            address_json = [{
+              "moniker" => "340116",
+              "uprn" => "340116",
+              "lines" => ["NATURAL ENGLAND", "DEANERY ROAD"],
+              "town" => "BRISTOL",
+              "postcode" => "BS1 5AH",
+              "easting" => "358205",
+              "northing" => "172708",
+              "country" => "",
+              "dependentLocality" => "",
+              "dependentThroughfare" => "",
+              "administrativeArea" => "BRISTOL",
+              "localAuthorityUpdateDate" => "",
+              "royalMailUpdateDate" => "",
+              "partial" => "NATURAL ENGLAND, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH",
+              "subBuildingName" => "",
+              "buildingName" => "HORIZON HOUSE",
+              "thoroughfareName" => "DEANERY ROAD",
+              "organisationName" => "NATURAL ENGLAND",
+              "buildingNumber" => "",
+              "postOfficeBoxNumber" => "",
+              "departmentName" => "",
+              "doubleDependentLocality" => ""
+            }].to_json
+
+            stub_request(:get, url)
+              .to_return(status: 200, body: address_json)
+          end
 
           it "returns a successful response" do
             response = described_class.run(postcode)
+
+            expect(a_request(:get, url)).to have_been_made.at_most_once
+
             expect(response).to be_a(Response)
             expect(response.successful?).to eq(true)
             expect(response.results).not_to be_empty
@@ -26,14 +58,20 @@ module DefraRuby
       end
 
       context "when the postcode is invalid" do
-        context "because it is not found" do
-          before { VCR.insert_cassette("os_places_postcode_invalid_no_match") }
-          after { VCR.eject_cassette }
+        let(:url) { "http://localhost:8005/addresses?postcode=#{postcode}" }
 
+        context "because it is not found" do
           let(:postcode) { "BS1 9XX" }
+          before do
+            stub_request(:get, url)
+              .to_return(status: 400, body: '{"error":{"statuscode":400,"message":"Parameters are not valid"}}')
+          end
 
           it "returns a failed response" do
             response = described_class.run(postcode)
+
+            expect(a_request(:get, url)).to have_been_made.at_most_once
+
             expect(response).to be_a(Response)
             expect(response).to_not be_successful
             expect(response.results).to be_empty
@@ -42,13 +80,18 @@ module DefraRuby
         end
 
         context "because it is blank" do
-          before { VCR.insert_cassette("os_places_postcode_invalid_blank") }
-          after { VCR.eject_cassette }
+          before do
+            stub_request(:get, url)
+              .to_return(status: 400, body: '{"errors":["query param postcode may not be empty"]}')
+          end
 
           let(:postcode) { "" }
 
           it "returns a failed response" do
             response = described_class.run(postcode)
+
+            expect(a_request(:get, url)).to have_been_made.at_most_once
+
             expect(response).to be_a(Response)
             expect(response).to_not be_successful
             expect(response.results).to be_empty
