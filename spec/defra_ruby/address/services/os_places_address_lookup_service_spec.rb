b606e9385881b1c208967cf2d0554d4b285d79f1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-10-21 20:16:12 +0100

    Refactor let and before blocks
    With a bit of refactoring we can reduce the number of let and before blocks scattered throughout our tests.

--- spec/defra_ruby/address/services/os_places_address_lookup_service_spec.rb
+++ spec/defra_ruby/address/services/os_places_address_lookup_service_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require "rest-client"
 
 module DefraRuby
   module Address
@@ -9,41 +8,40 @@ module DefraRuby
       describe "#run" do
         before do
           DefraRuby::Address.configure { |c| c.host = "http://localhost:8005/" }
+          stub_request(:get, url).to_return(status: code, body: body)
+        end
+
+        let(:postcode) { "BS1 5AH" }
+        let(:url) { "http://localhost:8005/addresses?postcode=#{postcode}" }
+        let(:code) { 200 }
+        let(:body) do
+          [{
+            "moniker" => "340116",
+            "uprn" => "340116",
+            "lines" => ["NATURAL ENGLAND", "DEANERY ROAD"],
+            "town" => "BRISTOL",
+            "postcode" => "BS1 5AH",
+            "easting" => "358205",
+            "northing" => "172708",
+            "country" => "",
+            "dependentLocality" => "",
+            "dependentThroughfare" => "",
+            "administrativeArea" => "BRISTOL",
+            "localAuthorityUpdateDate" => "",
+            "royalMailUpdateDate" => "",
+            "partial" => "NATURAL ENGLAND, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH",
+            "subBuildingName" => "",
+            "buildingName" => "HORIZON HOUSE",
+            "thoroughfareName" => "DEANERY ROAD",
+            "organisationName" => "NATURAL ENGLAND",
+            "buildingNumber" => "",
+            "postOfficeBoxNumber" => "",
+            "departmentName" => "",
+            "doubleDependentLocality" => ""
+          }].to_json
         end
 
         context "when the postcode is valid" do
-          let(:url) { "http://localhost:8005/addresses?postcode=#{postcode}" }
-          let(:postcode) { "BS1 5AH" }
-
-          before do
-            address_json = [{
-              "moniker" => "340116",
-              "uprn" => "340116",
-              "lines" => ["NATURAL ENGLAND", "DEANERY ROAD"],
-              "town" => "BRISTOL",
-              "postcode" => "BS1 5AH",
-              "easting" => "358205",
-              "northing" => "172708",
-              "country" => "",
-              "dependentLocality" => "",
-              "dependentThroughfare" => "",
-              "administrativeArea" => "BRISTOL",
-              "localAuthorityUpdateDate" => "",
-              "royalMailUpdateDate" => "",
-              "partial" => "NATURAL ENGLAND, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH",
-              "subBuildingName" => "",
-              "buildingName" => "HORIZON HOUSE",
-              "thoroughfareName" => "DEANERY ROAD",
-              "organisationName" => "NATURAL ENGLAND",
-              "buildingNumber" => "",
-              "postOfficeBoxNumber" => "",
-              "departmentName" => "",
-              "doubleDependentLocality" => ""
-            }].to_json
-
-            stub_request(:get, url)
-              .to_return(status: 200, body: address_json)
-          end
 
           it "returns a successful response" do
             response = described_class.run(postcode)
@@ -55,47 +53,40 @@ module DefraRuby
             expect(response.results).not_to be_empty
           end
         end
-      end
 
-      context "when the postcode is invalid" do
-        let(:url) { "http://localhost:8005/addresses?postcode=#{postcode}" }
+        context "when the postcode is invalid" do
+          let(:code) { 400 }
 
-        context "because it is not found" do
-          let(:postcode) { "BS1 9XX" }
-          before do
-            stub_request(:get, url)
-              .to_return(status: 400, body: '{"error":{"statuscode":400,"message":"Parameters are not valid"}}')
+          context "because it is not found" do
+            let(:postcode) { "BS1 9XX" }
+            let(:body) { '{"error":{"statuscode":400,"message":"Parameters are not valid"}}' }
+
+            it "returns a failed response" do
+              response = described_class.run(postcode)
+
+              expect(a_request(:get, url)).to have_been_made.at_most_once
+
+              expect(response).to be_a(Response)
+              expect(response).to_not be_successful
+              expect(response.results).to be_empty
+              expect(response.error).to_not be_nil
+            end
           end
 
-          it "returns a failed response" do
-            response = described_class.run(postcode)
+          context "because it is blank" do
+            let(:postcode) { "" }
+            let(:body) { '{"errors":["query param postcode may not be empty"]}' }
 
-            expect(a_request(:get, url)).to have_been_made.at_most_once
+            it "returns a failed response" do
+              response = described_class.run(postcode)
 
-            expect(response).to be_a(Response)
-            expect(response).to_not be_successful
-            expect(response.results).to be_empty
-            expect(response.error).to_not be_nil
-          end
-        end
+              expect(a_request(:get, url)).to have_been_made.at_most_once
 
-        context "because it is blank" do
-          before do
-            stub_request(:get, url)
-              .to_return(status: 400, body: '{"errors":["query param postcode may not be empty"]}')
-          end
-
-          let(:postcode) { "" }
-
-          it "returns a failed response" do
-            response = described_class.run(postcode)
-
-            expect(a_request(:get, url)).to have_been_made.at_most_once
-
-            expect(response).to be_a(Response)
-            expect(response).to_not be_successful
-            expect(response.results).to be_empty
-            expect(response.error).to_not be_nil
+              expect(response).to be_a(Response)
+              expect(response).to_not be_successful
+              expect(response.results).to be_empty
+              expect(response.error).to_not be_nil
+            end
           end
         end
       end
